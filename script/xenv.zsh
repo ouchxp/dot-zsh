bd3f47a6adf09f6458db815eed85cfe9fa94bdf9
Nan <ouchxp.me@gmail.com>
2019-10-16 16:19:58 +1300

    Prevent conflic from jenv managed Graal vs nodenv

--- script/xenv.zsh
+++ script/xenv.zsh
@@ -5,8 +5,12 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 [ -r ~/.pyenv ] && eval "$(pyenv init - --no-rehash)" || true
 # [ -r ~/.pyenv ] && eval "$(pyenv virtualenv-init -)" # this command is optional
 
-# Jenv
-[ -r ~/.jenv ] && eval "$(jenv init --no-rehash -)" || true
+# Jenv, Graal will provide a node and npm binary, but we do not want that
+[ -r ~/.jenv ] \
+    && eval "$(jenv init --no-rehash -)" \
+    && [ -f ~/.jenv/shims/node ] \
+    && /bin/rm ~/.jenv/shims/node \
+    && /bin/rm ~/.jenv/shims/npm || true
 
 # Nodenv
 [ -r ~/.nodenv ] && eval "$(nodenv init -)" || true
