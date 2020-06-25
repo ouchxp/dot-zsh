200192ae63db86143b65e45767bb5e8f50091101
Nan Wu <ouchxp.me@gmail.com>
2020-06-25 14:03:16 +1200

    Update brew and install script

---
+++ brew.sh
@@ -0,0 +1,8 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew tap homebrew/cask-drivers
+brew install zsh
+brew install git
+brew install yarn
+brew cask install hammerspoon
+brew cask install steelseries-exactmouse-tool
--- install.sh
+++ install.sh
@@ -10,3 +10,18 @@ if [ ! -f ~/.z ]; then
 fi
 ln -s ~/.zsh/.zshrc ~/.zshrc
 ln -s ~/.zsh/.zshenv ~/.zshenv
+ln -s ~/.zsh/.hushlogin ~/.hushlogin
+
+# Vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
+# KeyBindings
+cat << EOF > ~/Library/KeyBindings/DefaultKeyBinding.dict
+{
+    "\UF729"  = "moveToBeginningOfLine:";
+    "\UF72B"  = "moveToEndOfLine:";
+    "$\UF729" = "moveToBeginningOfLineAndModifySelection:";
+    "$\UF72B" = "moveToEndOfLineAndModifySelection:";
+}
+EOF
