e9a27c2fe2c1c3ca6fb9e51bdcc2afcfec7149d1
Nan Wu <ouchxp.me@gmail.com>
2020-06-29 19:25:14 +1200

    Add rbenv back

--- script/xenv.zsh
+++ script/xenv.zsh
@@ -1,19 +1,18 @@
-# Setup for pyenv
-# Set system python to the one from brew
+# pyenv
 [ -r ~/.pyenv ] && export PATH="/usr/local/opt/python/libexec/bin:$PATH" || true
 export PATH="$PYENV_ROOT/bin:$PATH"
 [ -r ~/.pyenv ] && eval "$(pyenv init - --no-rehash)" || true
 # [ -r ~/.pyenv ] && eval "$(pyenv virtualenv-init -)" # this command is optional
 
-# Jenv, Graal will provide a node and npm binary, but we do not want that
+# jenv, Graal will provide a node and npm binary, but we do not want that
 [ -r ~/.jenv ] \
     && eval "$(jenv init --no-rehash -)" \
     && [ -f ~/.jenv/shims/node ] \
     && /bin/rm ~/.jenv/shims/node \
     && /bin/rm ~/.jenv/shims/npm || true
 
-# Nodenv
+# nodenv
 [ -r ~/.nodenv ] && eval "$(nodenv init -)" || true
 
-# Rubyenv
-# [ -r ~/.rbenv ] && eval "$(rbenv init -)" || true
+# rbenv
+[ -r ~/.rbenv ] && eval "$(rbenv init -)" || true
