93d3bd43eedde634b719863a7345ee244563747d
Nan Wu <ouchxp.me@gmail.com>
2021-02-03 21:50:02 +1300

    remove unused script

--- script/aws.zsh
+++ script/aws.zsh
@@ -1,15 +1,4 @@
 ##
-# AWS - Helper Functions
-##
-if [[ -f ~/.awsAliases ]]; then
-    source ~/.awsAliases
-    alias awsall="_awsListAll"
-    alias awslp="_awsListProfile"
-    alias awsp="_awsSwitchProfile"
-    alias awswho="aws configure list"
-fi
-
-##
 # AWS Vault - Helper Functions
 ##
 _av_auto_unset() {
